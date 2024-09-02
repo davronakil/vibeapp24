91bbc2089500bd5f3a5ba4341442ef955959a8c3
PixelKraft LLC <dev@pixelkraft.net>
2024-09-01 21:47:34 -0500

    changed gitignore

---
+++ firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyACXLlgEV0trniWbTImDTDY2D6zRzbkt4Y",
+  authDomain: "vibeapp24.firebaseapp.com",
+  projectId: "vibeapp24",
+  storageBucket: "vibeapp24.appspot.com",
+  messagingSenderId: "1050605418268",
+  appId: "1:1050605418268:web:4e18d01aff5285c1e52cd9"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { app, db, auth, storage };
